ef573f5953a00d19fd027ceb184b10a4290b5ae6
Roman Nikrmajer <roman.nikrmajer@brandembassy.com>
2019-04-30 11:24:06 +0200

    games endpoint implemented. Tests not included for not due to lack of time.

--- src/modules/game/game.service.ts
+++ src/modules/game/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Axios, { AxiosResponse } from 'axios';
-import { ICheapSharkResponse } from './interfaces/cheapSharkResponse.interface';
+import { ICheapSharkInfoResponse, ICheapSharkDealResponse } from './interfaces/cheapSharkResponse.interface';
+import { IGame } from './interfaces/game.interface';
 
 @Injectable()
 export class GameService {
@@ -8,16 +9,45 @@ export class GameService {
   /**
    * Returns list of games
    */
-  public async getGames(): Promise<ICheapSharkResponse[]> {
-    const gamesList = await this.fetchGameInfo();
-    return gamesList.data;
+  public async getGames(): Promise<IGame[]> {
+    const gamesInfo: {data: ICheapSharkInfoResponse[]} = await this.fetchGameInfo();
+    const gamesDeals: {data: ICheapSharkDealResponse}[] = [];
+    const gamesList: IGame[] = [];
+
+    await Promise.all(gamesInfo.data.map(async (game) => {
+      const deal = await this.fetchGameInfoByDealId(game.dealID)
+      gamesDeals.push(deal)
+    }))
+
+    gamesDeals.forEach((deal, index) => {
+      const { gameInfo: {name, salePrice, releaseDate}, cheapestPrice: { price } } = deal.data;
+
+      // if game was not released yet we do not include release date into the response to prevent confusion
+      const convertedReleaseDate = releaseDate !== 0 ? new Date(releaseDate * 1000) : undefined;
+      gamesList.push({
+        name,
+        salePrice: Number(salePrice),
+        releaseDate: convertedReleaseDate,
+        cheapestPrice: Number(price)
+      })
+    })
+
+    return gamesList
   }
 
   private async fetchGameInfo(): Promise<AxiosResponse> {
     const res = await Axios.get('http://www.cheapshark.com/api/1.0/deals?storeID=1&desc=0&title=grand%20theft%20auto&pageSize=20');
-    if (res.status === 200) {
-      return res;
+    if (res.status !== 200) {
+      throw new Error('Error fetching games data');
     }
-    throw new Error('Error fetching games data');
+    return res;
+  }
+
+  private async fetchGameInfoByDealId(id: string): Promise<AxiosResponse> {
+    const res = await Axios.get(`http://www.cheapshark.com/api/1.0/deals?id=${id}`);
+    if (res.status !== 200) {
+      throw new Error(`Error fetching deal data. DealId: ${id}`)
+    }
+    return res
   }
 }
--- src/modules/game/interfaces/cheapSharkResponse.interface.ts
+++ src/modules/game/interfaces/cheapSharkResponse.interface.ts
@@ -1,4 +1,4 @@
-export interface ICheapSharkResponse {
+export interface ICheapSharkInfoResponse {
   readonly internalName: string;
   readonly title: string;
   readonly metacriticLink: string;
@@ -18,4 +18,40 @@ export interface ICheapSharkResponse {
   readonly lastChange: number;
   readonly dealRating: string;
   readonly thumb: string;
+}
+
+interface ICheapSharkCheaperStore {
+  readonly dealID: string,
+  readonly storeID: string,
+  readonly salePrice: string,
+  readonly retailPrice: string
+}
+
+interface ICheapSharkDealGameInfo {
+  readonly storeID: string,
+  readonly gameID: string,
+  readonly name: string,
+  readonly steamAppID: string,
+  readonly salePrice: string,
+  readonly retailPrice: string,
+  readonly steamRatingText: string,
+  readonly steamRatingPercent: string,
+  readonly steamRatingCount: string,
+  readonly metacriticScore: string,
+  readonly metacriticLink: string,
+  readonly releaseDate: number,
+  readonly publisher: string,
+  readonly steamworks: string,
+  readonly thumb: string
+}
+
+interface ICheapSharkCheapestPrice {
+  readonly price: string,
+  readonly date: number
+}
+
+export interface ICheapSharkDealResponse {
+  readonly gameInfo: ICheapSharkDealGameInfo,
+  readonly cheaperStores: ICheapSharkCheaperStore[],
+  readonly cheapestPrice: ICheapSharkCheapestPrice
 }
